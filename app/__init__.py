a257a4a95287d09312765bde10190ccdfc74c06a
KVOrange <kvvorange@gmail.com>
2021-05-29 04:17:22 +0500

    many changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,24 @@
 from flask import Flask
-from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
 
 import config
-from .models import db
+from .models import db, User
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config.Config)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.test' #добавить редирект
-    login_manager.init_app(app)
+    jwt = JWTManager(app)
+
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.filter_by(id=identity).one_or_none()
 
     db.init_app(app)
     with app.test_request_context():
         db.create_all()
-
-    from app.models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     import app.api.auth as auth
     import app.api.link as link
